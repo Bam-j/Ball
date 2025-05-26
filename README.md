5c481e69e39eccad11953da53efafb15e19767dd
bam <juhyun19981129@gmail.com>
2025-05-26 16:43:32 +0900

    첫 번째 커밋

---
+++ README.md
@@ -0,0 +1,6 @@
+# Ball
+
+> 만들고싶어서 만드는 프로젝트!
+
+- Ball은 웹 클리커 게임입니다.
+- [Cookie Clicker](https://cookieclicker.com/)로부터 영감을 받아서 제작되었습니다.
---
+++ backend/src/test/java/com/joo/ball/BallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joo.ball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
